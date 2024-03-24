8e440c144e10860a1ce224cc63baf1ae1ddea2fc
Eric Rodriguez <feric.rodriguez@gmail.com>
2024-03-24 02:28:24 -0300

    feat(logica): accumulated water units counter created

--- logica/3.js
+++ logica/3.js
@@ -17,6 +17,63 @@
    que retiene el agua.
  */
 
+/*
+
+[3, 1, 2, 4, 0, 1, 3]
+
+  💧💧💧💧💧💧💧
+  💧💧💧💧💧💧💧
+  💧💧💧🪣💧💧💧
+  🪣💧💧🪣💧💧🪣
+  🪣💧🪣🪣💧💧🪣
+  🪣🪣🪣🪣💧🪣🪣
+  Quedan 8
+
+
+  [5, 3, 7, 2, 7, 4, 5, 9, 1, 2]
+
+  💧💧💧💧💧💧💧🪣💧💧
+  💧💧💧💧💧💧💧🪣💧💧
+  💧💧🪣💧🪣💧💧🪣💧💧
+  💧💧🪣💧🪣💧💧🪣💧💧
+  🪣💧🪣💧🪣💧🪣🪣💧💧
+  🪣💧🪣💧🪣🪣🪣🪣💧💧
+  🪣🪣🪣💧🪣🪣🪣🪣💧💧
+  🪣🪣🪣🪣🪣🪣🪣🪣💧🪣
+  🪣🪣🪣🪣🪣🪣🪣🪣🪣🪣
+  Quedan 13
+
+*/
+
+
+
 const waterBlocks = (value) => {
-  return;
+
+  const blocks = [...value];
+
+  let acummWater = 0;
+
+  blocks.forEach((current, index) => {
+    let leftMax = 0;
+    let rightMax = 0;
+
+    const leftSlice = blocks.slice(0, index === blocks.length ? index + 1 : index);
+
+    if (leftSlice.length > 0) {
+      leftMax = Math.max(...leftSlice);
+    }
+
+    const rightSlice = blocks.slice(index + 1);
+
+    if (rightSlice.length > 0) {
+      rightMax = Math.max(...rightSlice);
+    }
+    const minHeight = Math.min(leftMax, rightMax);
+
+    acummWater += (minHeight - current < 0) ? 0 : minHeight - current;
+  });
+
+  return acummWater;
 };
+
+console.log(waterBlocks([5, 3, 7, 2, 7, 4, 5, 9, 1, 2]));
