9187ed1c9896661a73b66c855e9d3fe79f208d26
Eric Rodriguez <feric.rodriguez@gmail.com>
2024-03-24 21:24:25 -0300

    feat(logica): analysis of the auth handler functions in layout

--- logica/4.js
+++ logica/4.js
@@ -2,3 +2,44 @@
 //Pista, el codigo presentado handlea dos diferentes tipos de tokens (api token, session token)
 
 import image from "./code_example.png";
+
+// Para una lectura mas comoda se recomienda ingresar al documento de notion https://ericrodriguez.notion.site/Ejercicio-de-logica-An-lisis-de-c-digo-c03ed1808b634261913f579a97c6b4ca
+// Tambien es posible copiar el contenido en alguna herramienta que formatee markdown como Notion.
+
+/*
+
+# Funcionalidad
+
+## Auth
+
+- Se almacena el token de sesión y el token de la API en el localStorage.
+- Actualiza los tokens cuando es necesario.
+- Inicia el proceso de inicio de sesión si faltan los tokens.
+
+## Currency
+
+- Recupera información de la moneda actual.
+- Actualiza los valores de la moneda en el almacenamiento local.
+- Elegir una moneda preferida.
+
+## Otros
+
+- Incompleto. Obtener información del home y productos si se está autenticado.
+- Obtencion de datos geograficos y del usuario cuando está autenticado.
+
+## No muy claro
+
+- La obtencion del token con **getToken** utiliza variables de entorno para el email y la contraseña?
+
+# Mejoras
+
+- Al principio al utilizar hooks como el useState, usar parametros genericos para definir que tipo de dato se almacena en la variable de estado. Mas si el estado inicial es undefined o null.
+- Si tenemos un hook que nos ayuda a gestionar el localStorage como el “useLocalStorage” podría utilizarse para leer y almacenar el “currency”. Además de otras partes en el codigo en el cual la lectura
+- En las funciones que realizan peticiones a la API de Win se podría utilizar una instancia de axios para evitar repetir codigo como la obtención del “Base URL” de la variable de entorno NEXT_PUBLIC_API junto con version de la API que se utiliza, ya que en todo el código se hacen peticiones a “/api/v2”.
+- Continuando con axios, usar los parametros genericos que ofrecen los metodos de forma tal que tenga un tipado seguro.
+- Evitaría usar nombres de variables y/o funciones en el mismo scope que varíen solo por una letra o mayuscula/minuscula. Es el caso de la funcion setCurrency.
+- En la gestion de errores por ejemplo de funciones que hacen peticiones a la API de Win brindaría mensajes descriptivos al usuario, evitando colocar solo `console.log(error)` y asi evitar que en consola aparezcan mensajes de error del servidor.
+- Las funciones que realizan peticiones de recursos, autenticacion, etc., podrían estar en archivos separados organizados por dominios. Por ejemplo las peticiones de Currency, Auth, Products, Geo.
+- El SessionContext.Provider recibe una gran variedad de valores que podría separarse en otros contextos y providers organizados por preocupacion/dominio.
+- Comenzaría a utilizar context + useReducer, con el patron redux teniendo en cuenta que tengo que gestionar el estado de recursos asociados a diferentes preocupaciones/dominios.
+*/
