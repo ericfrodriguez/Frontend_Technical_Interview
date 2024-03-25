5a4ce39393539ffa4a227e25cce85563e057573e
Eric Rodriguez <feric.rodriguez@gmail.com>
2024-03-25 17:39:02 -0300

    feat(mobile): tabs styled

--- mobile-app/app/(tabs)/_layout.jsx
+++ mobile-app/app/(tabs)/_layout.jsx
@@ -1,23 +1,57 @@
 import { Tabs } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import Octicon from '@expo/vector-icons/Octicons'
+
+import { colors } from "../../theme/theme";
 
 const TabsLayout = () => {
     return (
-        <Tabs>
-            <Tabs.Screen
-                name="index"
-                options={{
-                    headerShown: false,
-                }}
-            />
-            <Tabs.Screen
-                name="search/index"
-                options={{
-                    headerTitle: "Search",
-                    title: "Search",
-                }}
-            />
-        </Tabs>
+        <>
+            <StatusBar style="auto" />
+            <Tabs screenOptions={{
+                ...tabBarStyles,
+            }}>
+                <Tabs.Screen
+                    name="index"
+                    options={{
+                        headerShown: false,
+                        title: 'Inicio',
+                        tabBarIcon: ({ color, size, focused }) => <Octicon name="home" color={focused ? colors.primary : 'white'} size={size} />
+                    }}
+                />
+                <Tabs.Screen
+                    name="search/index"
+                    options={{
+                        headerTitle: "Buscar",
+                        title: "Buscar",
+                        tabBarIcon: ({ color, size, focused }) => <Octicon name="search" color={focused ? colors.primary : 'white'} size={size} />
+                    }}
+                />
+            </Tabs>
+        </>
     );
 };
 
 export default TabsLayout;
+
+const tabBarStyles = {
+    tabBarActiveTintColor: colors.primary,
+    tabBarStyle: {
+        height: 68,
+        backgroundColor: 'black',
+        display: 'flex',
+        alignItems: 'center',
+        paddingBottom: 12,
+        borderTopWidth: 1,
+        borderTopColor: '#585858'
+    },
+    tabBarLabelStyle: {
+        fontSize: 12,
+        fontWeight: "normal",
+        color: colors.text,
+    },
+    tabBarItemStyle: {
+        flex: 0,
+        marginHorizontal: 25
+    },
+}
---
+++ mobile-app/theme/theme.js
@@ -0,0 +1,9 @@
+
+export const colors = {
+    primary: "#64C883",
+    secondary: "#C9A555",
+    text: "#FFFFFF",
+
+    background: "#171717",
+    cardBackground: "#0A0A0A",
+}
