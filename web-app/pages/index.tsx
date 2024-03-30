c29de9daff1f2c727b4334dcf96dd9c6ba77e8ab
Eric Rodriguez <feric.rodriguez@gmail.com>
2024-03-30 20:30:52 -0300

    feat(web): search page created

--- web-app/pages/index.tsx
+++ web-app/pages/index.tsx
@@ -1,10 +1,10 @@
 import Layout from '@/components/layouts/Layout';
 
-export default function Home() {
+export default function HomePage() {
   return (
     <>
       <Layout
-        title="Win Frontend Challenge"
+        title="Inicio - Win Frontend Challenge"
         description="Front End Technical Interview"
       >
         <h1>Hello world</h1>
---
+++ web-app/pages/search.tsx
@@ -0,0 +1,13 @@
+import Layout from "@/components/layouts/Layout";
+
+
+export default function SearchPage() {
+  return (
+    <Layout
+      title="Buscar - Win Frontend Challenge"
+      description="Front End Technical Interview"
+    >
+      <h1>Search Page</h1>
+    </Layout>
+  );
+}
